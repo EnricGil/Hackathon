30381b2922958e6324a28eb085136215652907e1
Miguel Gil <mgildev@gmail.com>
2017-09-30 01:33:36 +0200

    mas cosas

---
+++ app/src/main/java/eventmanager/ndcs/hackathon/myapplication/sqlite/database/SQLiteController.java
@@ -0,0 +1,45 @@
+package eventmanager.ndcs.hackathon.myapplication.sqlite.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import eventmanager.ndcs.hackathon.myapplication.Constants;
+import eventmanager.ndcs.hackathon.myapplication.application.EventmanagerApplication;
+import eventmanager.ndcs.hackathon.myapplication.model.User;
+
+/**
+ * Created by miguel on 30/9/17.
+ */
+
+public class SQLiteController {
+
+    private Context context;
+    private SQLiteGestor bdg;
+    private SQLiteDatabase bd;
+
+    public SQLiteController(Context context){
+        this.context = context;
+    }
+
+    public User getUserData(String email){
+        openReadableBD();
+        //Revisad esta consulta, no sé si es exactamente así
+        Cursor rs = bd.rawQuery("SELECT * FROM USER WHERE email == "+email,null);
+        if (rs.moveToNext())
+            return new User.Builder().id(rs.getString(1)).nombre(rs.getString(2)).apellidos(rs.getString(3)).email(rs.getString(4)).telefono(rs.getString(5)).admin(rs.getInt(6)==1).pass(rs.getString(7)).build();
+        else
+            return null;
+    }
+
+    private void openReadableBD(){
+        bdg = new SQLiteGestor(this.context, Constants.DATABASE_PATH, null, 1);
+        bd = bdg.getReadableDatabase();
+    }
+
+    private void openWritableBD(){
+        bdg = new SQLiteGestor(this.context, Constants.DATABASE_PATH, null, 1);
+        bd = bdg.getWritableDatabase();
+    }
+
+}
